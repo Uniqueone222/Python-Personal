d663e56d77d10732cee8b6089156a9eda800cac2
Uniqueone222 <eklavyaagrawal777@gmail.com>
2025-09-11 01:50:01 +0530

    push

--- AI-ML Club/Class 1/Session-1.py
+++ AI-ML Club/Class 1/Session-1.py
@@ -19,7 +19,7 @@ while True:
     if hand and hand[0]['type'] == 'Right':
         fingers = detector.fingersUp(hand[0])
         totalFingers = fingers.count(1)
-        cv2.putText(img, f'Fingers: {totalFingers}', (50,50), cv2.FONT_HERSHEY_PLAIN, 2, (0,255,0))
+        cv2.putText(img, f'Fingers: {totalFingers}', (50,50), cv2.FONT_HERSHEY_PLAIN, 2, (0,0,255))
         if totalFingers == 5:
             pyautogui.keyDown('right')
             pyautogui.keyUp('left')
@@ -27,4 +27,4 @@ while True:
             pyautogui.keyDown('left')
             pyautogui.keyUp('right')
     cv2.imshow('Livefeed',img)
-    cv2.waitKey(1)
+    cv2.waitKey(10)
